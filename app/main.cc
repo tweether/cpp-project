dd6b3ad5488b73270fb0f7166439b9186873366e
喵喵大人 <support@catlib.io>
2020-05-11 15:26:54 +0800

    Remove unsafly code.(CWE-78) (#5)

--- app/main.cc
+++ app/main.cc
@@ -20,7 +20,6 @@ int main() {
             << "."
             << PROJECT_VERSION_TWEAK
             << std::endl;
-  std::system("cat ../LICENSE");
 
   // Bring in the dummy class from the example source,
   // just to show that it is accessible from main.cpp.
